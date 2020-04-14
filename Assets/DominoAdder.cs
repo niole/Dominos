e6ef33a49714879bb45d6d645db48040157363c9
niole <niolenelson@gmail.com>
2020-04-14 08:39:14 -0700

    New Dominos have right y pos

--- Assets/DominoAdder.cs
+++ Assets/DominoAdder.cs
@@ -23,7 +23,8 @@ public class DominoAdder : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(mousePos);
             RaycastHit hit;
             if(Physics.Raycast(ray,out hit,1000f)) {
-                GameObject newParticle = Instantiate(particle, hit.point, Quaternion.identity);
+                Vector3 hitLocation = new Vector3(hit.point.x, 0.17f, hit.point.z);
+                GameObject newParticle = Instantiate(particle, hitLocation, Quaternion.identity);
                 particle = newParticle;
                 //or for tandom rotarion use Quaternion.LookRotation(Random.insideUnitSphere)
             }
