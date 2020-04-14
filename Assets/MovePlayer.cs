fde5aab0507fd86812b96e89bf9f239f089d60ef
niole <niolenelson@gmail.com>
2020-04-14 09:13:16 -0700

    Can knock dominos over

---
+++ Assets/MovePlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public Rigidbody player;
+
+    public float speed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float vert = ApplySpeed(Input.GetAxis("Vertical"));
+        float horiz = ApplySpeed(Input.GetAxis("Horizontal"));
+
+        player.AddForce(horiz, 0f, vert, ForceMode.VelocityChange);
+
+    }
+
+    float ApplySpeed(float output)
+    {
+        return output * speed * Time.deltaTime;
+    }
+}
